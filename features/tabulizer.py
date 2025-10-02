23e847d9d0ebc5898f1c2407ecaebd68167ddd21
Zuhair binFarhan <zuhairfarhan@gmail.com>
2025-10-02 15:21:20 +0500

    added time based features to the dataframe.

--- features/tabulizer.py
+++ features/tabulizer.py
@@ -1,13 +1,19 @@
 import glob
 import pandas as pd
+from time_based_features import add_time_based_features
 from tabularize_raw_data import tabularize_raw_data
 
 def tabularize_all_files():
     all_files = glob.glob("raw_data/*.json")
     dfs = [tabularize_raw_data(f) for f in all_files]
-    return pd.concat(dfs, ignore_index=True) if dfs else None
+    df = pd.concat(dfs, ignore_index=True) if dfs else None
+    df = df.drop_duplicates(subset=['timestamp_utc', 'city']) if df is not None else None
+    return df
 
 if __name__ == "__main__":
     df = tabularize_all_files()
     if df is not None:
+        df = add_time_based_features(df)
         print(df.head())
+        print("-"*21)
+        print("The features of the dataframe include: \n", df.columns)
---
+++ features/time_based_features.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def add_time_based_features(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    df['timestamp_utc'] = pd.to_datetime(df['timestamp_utc'], utc=True)
+    df['hour'] = df['timestamp_utc'].dt.hour
+    df['day_of_week'] = df['timestamp_utc'].dt.dayofweek # 0 = Monday, 6 = Sunday
+    df['day_of_month'] = df['timestamp_utc'].dt.day
+    df['month'] = df['timestamp_utc'].dt.month
+    return df
+
